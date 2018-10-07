ab7d90504941cec7b33e4740aedae8770b466eda
kazuki229 <tsuzuku.k@gmail.com>
2018-10-07 15:57:54 +0900

    :sparkles: add creation user

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,7 +8,7 @@ if (!admin.apps.length) {
   admin.initializeApp();
 }
 
-const getSigningKey = (client: jwksClient.JwksClient, kid: string): Promise<string> => {
+function getSigningKey(client: jwksClient.JwksClient, kid: string): Promise<string> {
   return new Promise((resolve, reject) => {
     client.getSigningKey(kid, (error, key) => {
       if (error) {
@@ -21,7 +21,7 @@ const getSigningKey = (client: jwksClient.JwksClient, kid: string): Promise<stri
   });
 };
 
-const verify = (idToken, signingKey, verifyOptions): Promise<string | object> => {
+function verify(idToken, signingKey, verifyOptions): Promise<string | object> {
   return new Promise((resolve, reject) => {
     jwt.verify(idToken, signingKey, verifyOptions, (error, decoded) => {
       if (error) {
@@ -30,6 +30,43 @@ const verify = (idToken, signingKey, verifyOptions): Promise<string | object> =>
       }
       resolve(decoded);
     });
+  });
+}
+
+/**
+ * Look up Firebase user based on Yahoo! JAPAN's mid. If the Firebase user does not exist,
+ + fetch Yahoo! JAPAN profile and create a new Firebase user with it.
+ *
+ * @returns {Promise<UserRecord>} The Firebase user record in a promise.
+ */
+function getFirebaseUser(uid, accessToken) {
+  const getProfileOptions = {
+    url: 'https://userinfo.yahooapis.jp/yconnect/v2/attribute',
+    headers: {
+      Authorization: 'Bearer ' + accessToken
+    },
+  }
+
+  return admin.auth().getUser(uid).catch(error => {
+    if (error.code === 'auth/user-not-found') {
+      return rp(getProfileOptions).then(response => {
+        const json = JSON.parse(response);
+        const displayName: string = json.nickname;
+        const photoURL: string = json.picture;
+        const email: string = json.email;
+        const emailVerified: boolean = json.email_verified;
+        // Create a new Firebase user with Yahoo! JAPAN profile and return it
+        return admin.auth().createUser({
+          uid: uid,
+          displayName: displayName,
+          photoURL: photoURL,
+          email: email,
+          emailVerified: emailVerified
+        });
+      });
+    }
+    // If error other than auth/user-not-found occurred, fail the whole login process
+    throw error;
   });
 }
 
@@ -82,10 +119,17 @@ export const issueToken = functions.https.onRequest(async (request, response) =>
   });
 
   const uid = 'yahoojapan:' + jwtDecoded['sub'];
-  const customToken = await admin.auth().createCustomToken(uid).catch(error => {
+  const user: admin.auth.UserRecord = await getFirebaseUser(uid, json.access_token).catch(error => {
     // TODO: error handling
     console.log(error);
   });
+  console.log(user);
+
+  const customToken = await admin.auth().createCustomToken(user.uid)
+    .catch(error => {
+      // TODO: error handling
+      console.log(error);
+    });
 
   response.contentType('application/json');
   response.send(JSON.stringify({
